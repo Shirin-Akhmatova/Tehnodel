0880377a3b838ed694898618d63735b3a071edb7
Arsenoverkill <arsenprogrammer007@gmail.com>
2025-08-16 17:40:10 +0600

    Merge pull request #2 from Shirin-Akhmatova/header
    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 import Map from "./components/Map/Map";
 import ReviewsCarousel from "./components/ReviewsCarousel/ReviewsCarousel";
 
 function App() {
   return (
     <>
+      <Header/>
       <ReviewsCarousel />
       <Map />
       <Footer />
