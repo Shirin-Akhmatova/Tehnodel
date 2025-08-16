3fcdcd0d4bcd9196d078a1c62924dacf75d2da4a
Shirin <kamisatosaan>
2025-08-16 18:47:29 +0600

    about_us is done

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Hero from "./components/Hero/Hero";
 import Header from "./components/header/Header";
 import Map from "./components/Map/Map";
 import ReviewsCarousel from "./components/ReviewsCarousel/ReviewsCarousel";
+import AboutUs from "./components/aboutUs/AboutUs";
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <Hero />
+      <AboutUs />
       <ReviewsCarousel />
       <Map />
       <Footer />
